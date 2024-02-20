0ed6c5c308278bbaddaf2db9911c4ca46fa2cedc
Gutierry <aleguti1000@hotmail.com>
2024-02-20 10:44:01 -0300

    first commit

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,7 @@
+export * from './colors';
+export * from './fonts';
+export * from './font-weights';
+export * from './font-sizes';
+export * from './radious';
+export * from './line-heights';
+export * from './spaces';
